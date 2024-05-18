cbebb201a6a6f2427548a994a4d30778d178e3f1
Navin GV <gnavin@amazon.com>
2024-05-18 22:32:38 +0530

    adding exceptions

---
+++ src/main/kotlin/graviton/exceptions/CreditsPricePackageNotFoundException.kt
@@ -0,0 +1,3 @@
+package graviton.exceptions
+
+class CreditsPricePackageNotFoundException(packageName: String) : Exception(packageName)
---
+++ src/main/kotlin/graviton/exceptions/ServiceNotFoundException.kt
@@ -0,0 +1,3 @@
+package graviton.exceptions
+
+class ServiceNotFoundException(serviceName: String) : Exception(serviceName)
